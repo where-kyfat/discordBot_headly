4d80b913bebec60fee4da08f193624d9d6b59ab6
AndreyTepin <47591725+AndreyTepin@users.noreply.github.com>
2022-03-04 13:33:57 +0300

    The commit implemented the deletion of messages with a mention, in 10 seconds after they were sent, from all users, except for the Bot.

--- main.py
+++ main.py
@@ -1,50 +1,32 @@
 import discord
-from operations import *
-from models import *
-#from discord.ext import commands
+from discord.ext import commands
 from config import settings
 
-
-if __name__ == '__main__':
-    try:
-        db.connect()
-        Messages.create_table()
-    except peewee.InternalError as px:
-        print(str(px))
-#bot = commands.Bot(command_prefix=settings['prefix'])
-
-client = discord.Client()
+bot = commands.Bot(command_prefix=settings['prefix'])
 
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+def is_me(ctx):
+    return ctx.message.author == bot.user
 
 
-@client.event
+@bot.command()
+async def hello(ctx):
+    author = ctx.message.author
+
+    await ctx.send(f'hello, {author.mention}!')
+
+
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    await bot.process_commands(message)
+    if is_me:
         return
 
+    if message.mentions != []:
+        await message.delete(delay=10)
 
-    #for item in Messages.select().where(
-       # Messages.created_at.minute < pw.datetime.datetime.now().minute
-   # ):
-
-        #await message.channel.fetch(item.dis_id_str).then(message => message.delete() )
+bot.run(settings['token'])
 
 
-    if message.content.startswith('$hello'):
-        add_message(message.id, message.content, message.author)
-        if (pw.datetime.datetime.now().minute - find_message(message.id).created_at.minute) >= 3:
-            await message.channel.send(str(find_message(message.id).message) + " " + str(find_message(message.id).created_at))
 
 
-#@bot.command()
-#async def hello(ctx):
- #   author = ctx.message.author
-#
- #   await ctx.send(f'hello, {author.mention}!')
-#
-#bot.run(settings['token'])
-client.run(settings['token'])
